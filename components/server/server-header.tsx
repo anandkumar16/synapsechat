b58443ac540056b3def926d15522104b7ef1731c
anandkumar16 <anandyadav16022004@gmail.com>
2025-03-08 20:09:24 +0530

    minor

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -30,7 +30,7 @@ const ServerHeader = ({
            className="w-full text-md font-semibold px-4 flex items-center h-12 border-neutral-200 dark:border-neutral-800  hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
            >
             {server.name}
-            <ChevronDown className="h-5 w-5 ml-auto"/>
+            <ChevronDown className="h-5 w-5"/>
             </button> 
         </DropdownMenuTrigger>
         <DropdownMenuContent
