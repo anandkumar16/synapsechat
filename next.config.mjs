d9c5f98a65df02f89723c69b1847e8a1343d0303
anandkumar16 <anandyadav16022004@gmail.com>
2025-06-23 17:16:42 +0530

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack:(config)=>{
-      config.externals.push({
-        "utf-8-validate": "commonjs utf-8-validate",
-        "bufferutil": "commonjs bufferutil",
-      });
-      return config;
-    },
-    images: {
-      remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'utfs.io',
-            pathname: '/**', 
-          },
-        {
-          protocol: 'https',
-          hostname: 'uploadthing.com',
-        },
-      ],
-    },
-    experimental: {
-      serverActions: true,
-    }
-  };
-  
-  export default nextConfig;
-  
+  output: "standalone", 
+
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      "bufferutil": "commonjs bufferutil",
+    });
+    return config;
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
